d218d9dc2f5b9226476be350406816fcac60ab08
Zinin Ilya <metal-fox8@yandex.ru>
2023-02-27 21:51:08 +0300

    апргейд аводополнения. добавляет сообщение о поиске результатов и скрытие при расфокусе

--- script/autocomplite.js
+++ script/autocomplite.js
@@ -5,26 +5,47 @@ const { inputSearchCoin, } = constant;
 
 export const autocomplite = (input, array) => {
     let list = input.nextElementSibling;
-    let val = input.value.trim().toLowerCase()
-    if (val !== ''){
-        let searchResultHTML = [];
-        list.classList.remove('close');
-        array.forEach(e => {
-            if (e.toLowerCase().search(val) != -1){
-                let valStart = e.toLowerCase().indexOf(val);
-                let valEnd = valStart + val.length;
-                let strPre = e.slice(0, valStart);
-                let strVal = e.slice(valStart, valEnd);
-                let strPost = e.slice(valEnd);
-                searchResultHTML.push(`
+    const search = (input, array) => {
+        let val = input.value.trim().toLowerCase()
+        if (val !== ''){
+            let searchResultHTML = [];
+            list.classList.remove('close');
+            array.forEach(e => {
+                if (e.toLowerCase().search(val) != -1){
+                    let valStart = e.toLowerCase().indexOf(val);
+                    let valEnd = valStart + val.length;
+                    let strPre = e.slice(0, valStart);
+                    let strVal = e.slice(valStart, valEnd);
+                    let strPost = e.slice(valEnd);
+                    searchResultHTML.push(`
                     <li class="list-group-item">
-                        ${strPre}<b>${strVal}</b>${strPost}
+                    ${strPre}<b>${strVal}</b>${strPost}
                     </li>`
-                );
+                    );
+                }
+            });
+            if (searchResultHTML.length != 0){
+                list.innerHTML = '<li class="list-group-item">Загрузка результатов...</li>';
+                setTimeout(() => {
+                    list.innerHTML = searchResultHTML.join('');
+                }, 0);
+            } else {
+                list.innerHTML = '<li class="list-group-item">Ничего не найдено :(</li>';
             }
-        });
-        list.innerHTML = searchResultHTML.join('');
-    } else {
-        list.classList.add('close');
+        } else {
+            list.classList.add('close');
+        }
     }
+    // ===============================================================
+    input.addEventListener('input', () => {
+        search(input, array);
+    });
+    input.addEventListener('focus', () => {
+        if (input.value.trim() !== ''){
+            list.classList.remove('close');
+        }
+    })
+    input.addEventListener('blur', () => {
+        list.classList.add('close');
+    });
 }
